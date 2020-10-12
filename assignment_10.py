29e0f26bfc275def1a48610f3ff90208996a8640
Satendra Pahuja <69447953+Satendrapahuja@users.noreply.github.com>
2020-10-12 18:52:41 +0530

    Add files via upload

---
+++ assignment_10.py
@@ -0,0 +1,8 @@
+numbers = [2, 3, 3, 4, 6, 27, 72, 76, 76]
+duplicate = list(set(numbers))
+print(duplicate)
+
+
+
+
+
---
+++ assignment_8.py
@@ -0,0 +1,6 @@
+list_1 = [144, 23, 53, 64, 76, 85, 342, 56, 23]
+
+list_1.sort()
+print(f'second largest number is :- ', list_1[-2])
+print(f'second smallest number is :-', list_1[+1])
+
